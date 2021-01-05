9f13bcb39cc31705b5431b1bf3030f1833b1b5c3
milanmdev <keiki08@outlook.com>
2021-01-05 16:50:57 -0600

    Add Servers API

---
+++ commands/api/servers.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const chatwind = require("chatwind.js");
+//packges or const here!
+module.exports = {
+  name: "servers",
+  aliases: ["servers", "s"],
+  category: "api",
+  description: "Lists all of the Chatwind servers",
+  usage: "servers",
+  dev: false,
+  run: async (client, message, args) => {
+
+    let servers = await chatwind.servers();
+    let serversMessage = `\`\`\`${servers.join('\n')}\`\`\``
+
+    let embed = new Discord.MessageEmbed()
+      .setTitle("Chatwind Servers")
+      .setDescription(serversMessage)
+
+    message.channel.send(embed)
+
+  }
+}
